b7508a9768aea6c1d46acd98dae919fa93097068
wifec <olga.alonina@gmail.com>
2022-05-29 18:16:16 -0400

    Palindrome task //group1 Alonina Olga

--- src/test/java/Group1/Group1_Java_Tasks.java
+++ src/test/java/Group1/Group1_Java_Tasks.java
@@ -1,6 +1,24 @@
 package Group1;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Group1_Java_Tasks {
-  //Alonina Olga
-  //Ahmad Jamil Rahmaty @ group20
+    //Alonina Olga i will get line 4 till 50
+    public static boolean checkPalindrome(String str) {
+        String[] unmod = str.split( "" );
+        String[] temp = str.split( "" );
+        Collections.reverse( Arrays.asList( temp ) );
+        if (Arrays.equals( unmod, temp )) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( checkPalindrome( "watermelon" ) );
+        System.out.println( checkPalindrome( "racecar" ) );
+    }
+
+    //Ayse Demirhan
 }
