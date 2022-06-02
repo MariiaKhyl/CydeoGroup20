302de4deee34ac1d72bf8de260fc7bb1109a1034
CristinaL <cristinabotnari@yahoo.com>
2022-06-02 12:11:18 -0500

    First Commit Cristina

--- src/test/java/Group3/Group3_Java_Tasks.java
+++ src/test/java/Group3/Group3_Java_Tasks.java
@@ -3,14 +3,77 @@ package Group3;
 public class Group3_Java_Tasks {
 
 
-    public static boolean isPalindrome(String str){
+    // LINES 7-150   --> Semih
+    // LINES 151-300 --> Cristina
+    // LINES 301-450 --> Igor
+
+
+    public static boolean isPalindrome(String str) {
 
         String reverse = "";
 
-        for (int i = str.length()-1 ; i >= 0 ; i--) {
+        for (int i = str.length() - 1; i >= 0; i--) {
 
             reverse += str.charAt(i);
         }
         return str.equalsIgnoreCase(reverse);
+
+    }
+
+    public static boolean PalindromeCheck(String str) {
+
+        String reverse = "";
+
+        for (int j = str.length() - 1; j >= 0; j--) {
+
+            reverse += str.charAt(j);
+        }
+        return str.equalsIgnoreCase(reverse);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
