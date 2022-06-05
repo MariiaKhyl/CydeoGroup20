bd4f9a8a882c9020946fb80a26c4b15d0188abff
Semih <komursemih1@gmail.com>
2022-06-05 11:01:28 -0400

    Second solution by Semih Group#3

--- src/test/java/Group3/Group3_Java_Tasks.java
+++ src/test/java/Group3/Group3_Java_Tasks.java
@@ -1,5 +1,7 @@
 package Group3;
 
+import java.util.ArrayList;
+
 public class Group3_Java_Tasks {
 
 
@@ -19,6 +21,162 @@ public class Group3_Java_Tasks {
         return str.equalsIgnoreCase(reverse);
 
     }
+
+
+
+
+    /*
+    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function,
+but instead be stored inside the array nums1. To accommodate this,
+nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+ and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+     */
+
+    public static int[] mergeArr(int[] arr,int[]arr2, int num1, int num2){
+
+        int [] result = new int[num1+num2];
+
+        for (int i = 0; i < num1 ; i++) {
+            result[i] = arr[i];
+        }
+        for (int i = num1, j =0; i <num1+num2 ; i++) {
+            result[i] = arr2[j];
+            j++;
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static boolean PalindromeCheck(String str) {
 
